98eab5b7b6478d4c10b25aca11ef829e19acb755
Ivan Marchenko <vanmaru.kun@gmail.com>
2022-07-20 00:51:06 +0300

    receiving a request and processing data. json parsing

---
+++ TestForDCT/Model/DataHandler.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestForDCT.ViewModel;
+
+namespace TestForDCT.Model
+{
+    class DataHandler
+    {
+        public void GetAssets()
+        {
+            GetRequest request = new("https://api.coincap.io/v2/assets");
+            request.Run();
+            string response = request.Response;
+            JObject json = JObject.Parse(response);
+        }
+    }
+}
